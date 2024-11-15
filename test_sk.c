74f78cbaed9c51134ebe9d9d9c00b2abaf0395ea
Shyliuli <139528661+Shyliuli@users.noreply.github.com>
2024-11-15 13:47:22 +0800

    Update test_sk.c

--- test_sk.c
+++ test_sk.c
@@ -10,13 +10,16 @@
 #define INPUT_TYPE_3 int
 // 可以更多....
 #define OUTPUT_TYPE int
+//函数返回值类型宏
+#define OUTPUT_TYPE_IS_NOT_VOID 1
+//如果函数不返回，定义为0，否则为1
 // 用于判断是否有 printf 调用
 INPUT_TYPE_1 input_args_1[TEST_NUM] = {1, 2}; // 输入变量_1
 INPUT_TYPE_2 input_args_2[TEST_NUM] = {2, 2}; // 输入变量_2
 INPUT_TYPE_3 input_args_3[TEST_NUM] = {3, 2}; // 输入变量_3
 //......可以有更多，记得改INPUT_ARGS_NUMS 和 INPUT_TYPE_*
-#if defined(OUTPUT_TYPE) &&OUTPUT_TYPE!=void
-OUTPUT_TYPE expected_output[TEST_NUM] = {6, 1}; 
+#if OUTPUT_TYPE_IS_NOT_VOID
+OUTPUT_TYPE expected_output[TEST_NUM] = {6, 6}; 
 #endif         // 期待返回变量
 #define PRINTF_MAX_ARGS 1000                             // printf 参数最大个数
 const char *expected_printf_input[PRINTF_MAX_ARGS] = {}; // 预期printf输入
@@ -27,7 +30,7 @@ const char *expected_printf_input[PRINTF_MAX_ARGS] = {}; // 预期printf输入
     A;                          \
     printf("\033[0m") // 设置颜色，背景黄前景红
 #define right_color(A)          \
-    printf("\033[42m\033[32m"); \
+    printf("\033[44m\033[32m"); \
     A;                          \
     printf("\033[0m")                   // 设置颜色，背景绿前景绿
 int is_broken = 0;                      // 是否测试失败
@@ -86,6 +89,9 @@ void myprintf(const char *format, ...)
         error_color(printf("Error at test %d:printf Expected '%s', but got '%s'\n",test_count,expected_printf_input[myprintf_count], buffer));
         is_broken = 1;
     }
+    else{
+        right_color(printf("Test %d:printf value passwd;\n"));
+    }
 
     myprintf_count++;
 }
@@ -97,7 +103,7 @@ void test_func()
         INPUT_TYPE_2 input_arg_2 = input_args_2[test_count];
         INPUT_TYPE_3 input_arg_3 = input_args_3[test_count];
         // 调用待测试函数
-        #if defined(OUTPUT_TYPE)&&OUTPUT_TYPE!=void
+        #if OUTPUT_TYPE_IS_NOT_VOID
             OUTPUT_TYPE output;
             switch (INPUT_ARGS_NUMS)
             {
@@ -121,11 +127,11 @@ void test_func()
             // 检查输出是否与预期一致
             if (output == expected_output[test_count])
             {
-                continue;
+                right_color(printf("Test %d return value passed\n", test_count));
             }
             else{
             
-                error_color(printf("Test %d failed: expected %d, but got %d\n", test_count, expected_output[test_count], output));
+                error_color(printf("Test %d failed:return value expected %d, but got %d\n", test_count, expected_output[test_count], output));
                 is_broken = 1;
             }
         
